eeeaeef3f8cce18d16b4c24b80b025ec540a807e
aguvai <145935152+aguvai@users.noreply.github.com>
2025-06-28 13:49:56 -0400

    Add styling for book cards
    The cards will be displayed using a grid layout.
    
    Also, added a yearPublished to book objects.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book (title, author, pages, read) {
+function Book (title, author, pages, read, yearPublished) {
   if (!new.target) {
     throw Error("Must use the new operator to call the consturctor!");
   }
@@ -9,6 +9,7 @@ function Book (title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.yearPublished = yearPublished
   this.uniqueID = crypto.randomUUID();
   
   this.info = function() {
@@ -16,12 +17,12 @@ function Book (title, author, pages, read) {
   }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new  Book(title, author, pages, read));
+function addBookToLibrary(title, author, pages, read, yearPublished) {
+    myLibrary.push(new  Book(title, author, pages, read, yearPublished));
 }
 
-addBookToLibrary("Cool book!", "He Who Shall Not Be Named", "199", false);
-addBookToLibrary("Random book...", "Random Author", "21542", true);
+addBookToLibrary("Cool book!", "Dr. Cool Author, PhD", "199", false, 2024);
+addBookToLibrary("Random book...", "Random Author", "21542", true, 1996);
 
 function createBookCard(book) {
     const cardContainer = document.querySelector(".card-container");
@@ -38,9 +39,25 @@ function createBookCard(book) {
     authorText.innerText = book.author;
     mainCard.appendChild(authorText);
 
+    const divider = document.createElement("div")
+    divider.classList.add("divider");
+    mainCard.appendChild(divider);
+
+    const infoContainer = document.createElement("div");
+    infoContainer.classList.add("info-container");
+    mainCard.appendChild(infoContainer);
+
     const pagesText = document.createElement("p");
     pagesText.innerText = `${book.pages} pages`;
-    mainCard.appendChild(pagesText);
+    infoContainer.appendChild(pagesText);
+
+    const infoDivider = document.createElement("div")
+    infoDivider.classList.add("info-divider");
+    infoContainer.appendChild(infoDivider);
+
+    const yearPublishedText = document.createElement("p");
+    yearPublishedText.innerText = `${book.yearPublished}`;
+    infoContainer.appendChild(yearPublishedText);
 }
 
 function displayBooks() {
